a60cc21298738ab99446bfc5723099f9b881a65c
yujie-121 <94167770+yujie-121@users.noreply.github.com>
2023-03-30 20:12:56 +0200

    add function

--- to-do.js
+++ to-do.js
@@ -1,3 +1,17 @@
 const addButtonElement = document.getElementById("add-button");
 console.log(addButtonElement);
+const inputValueElement = document.getElementById("add-input");
+console.log(inputValueElement);
+const myToDoListElement = document.getElementById("myToDoList");
+console.log(myToDoListElement);
+
 let toDoList = [];
+
+addButtonElement.onclick = function () {
+  const addListElement = document.createElement("li");
+  const span = document.createElement("span");
+  const text = document.createTextNode(inputValueElement.value);
+  span.appendChild(text);
+  addListElement.appendChild(span);
+  myToDoListElement.appendChild(addListElement);
+};
