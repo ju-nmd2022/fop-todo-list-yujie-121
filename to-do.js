b32a502277ddb294a59eb8c7a61f10c1b081b12a
yujie-121 <94167770+yujie-121@users.noreply.github.com>
2023-04-04 02:21:36 +0200

    solved mark and delete

--- to-do.js
+++ to-do.js
@@ -5,27 +5,56 @@ const myToDoListElement = document.getElementById("myToDoList");
 let toDoList = [];
 
 addButtonElement.onclick = function () {
-  const addListElement = document.createElement("li");
-  const span = document.createElement("span");
-  const text = document.createTextNode(inputValueElement.value);
-  span.appendChild(text);
-  addListElement.appendChild(span);
-  myToDoListElement.appendChild(addListElement);
-  inputValueElement.value = "";
+  if (inputValueElement.value.length > 0) {
+    const listContainerElement = document.createElement("div");
+    const li = document.createElement("li");
+    const toDoText = document.createTextNode(inputValueElement.value);
+    li.appendChild(toDoText);
+    listContainerElement.appendChild(li);
+    myToDoListElement.appendChild(listContainerElement);
+    inputValueElement.value = "";
 
-  //create mark button
-  const markButton = document.createElement("button");
-  markButton.innerHTML = "✔";
-  span.appendChild(markButton);
+    //create mark button
+    const markButton = document.createElement("button");
+    markButton.innerHTML = "✔️";
+    markButton.onclick = markElement;
+    listContainerElement.appendChild(markButton);
 
-  // //solusion 1(not working)
-  // const markButtonClick = markButton;
-  // markButtonClick.addEventListener.textDecoration = "line-through";
-  // //solusion 2(always working)
-  // markButton.onclick = myToDoListElement.style.textDecoration = "line-through";
-
-  // create delete button
-  const deleteButton = document.createElement("button");
-  deleteButton.innerHTML = "❌";
-  span.appendChild(deleteButton);
+    //create delet button
+    const deleteButton = document.createElement("button");
+    deleteButton.innerHTML = "✖️";
+    deleteButton.onclick = removeElement;
+    listContainerElement.appendChild(deleteButton);
+  }
 };
+
+function markElement() {
+  const element = this.parentNode;
+  element.style.textDecoration = "line-through";
+}
+
+function removeElement() {
+  const element = this.parentNode;
+  element.parentNode.removeChild(element);
+}
+
+// addButtonElement.onclick = function () {
+//   if (inputValueElement.value.length > 0) {
+//     const addListElement = document.createElement("li");
+//     const span = document.createElement("span");
+//     const text = document.createTextNode(inputValueElement.value);
+//     span.appendChild(text);
+//     addListElement.appendChild(span);
+//     myToDoListElement.appendChild(addListElement);
+//     inputValueElement.value = "";
+
+//     //create mark button
+//     const markButton = document.createElement("button");
+//     markButton.innerHTML = "✔️";
+
+//     span.appendChild(markButton);
+//     const deleteButton = document.createElement("button");
+//     deleteButton.innerHTML = "✖️";
+//     span.appendChild(deleteButton);
+//   }
+// };
