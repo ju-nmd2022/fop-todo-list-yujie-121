fde4a0d9bc33db781f8b741d97358550816021a6
yujie-121 <94167770+yujie-121@users.noreply.github.com>
2023-04-11 19:35:49 +0200

    add to local

--- to-do.js
+++ to-do.js
@@ -1,6 +1,7 @@
 let addButtonElement;
 let inputValueElement;
 let myToDoListElement;
+let saveItem;
 
 let toDoList = [];
 
@@ -8,6 +9,7 @@ function onLoadHandler() {
   addButtonElement = document.getElementById("add-button");
   inputValueElement = document.getElementById("input-field");
   myToDoListElement = document.getElementById("myToDoList");
+  displayListItem();
 
   addButtonElement.addEventListener("click", addToDoItemElement);
 
@@ -16,6 +18,7 @@ function onLoadHandler() {
   storedToDoList;
 
   function addToDoItemElement() {
+    saveItem = inputValueElement.value;
     if (inputValueElement.value.length > 0) {
       const listContainerElement = document.createElement("div");
       const li = document.createElement("li");
@@ -54,8 +57,9 @@ function removeElement() {
 
 function saveToDoLists() {
   let listItem = {
-    item: inputValueElement.value,
+    item: saveItem,
   };
+
   if (localStorage.listItem === undefined) {
     localStorage.listItem = JSON.stringify([]);
   }
@@ -69,8 +73,24 @@ function displayListItem() {
     let listItemArray = JSON.parse(localStorage.listItem);
 
     for (let list of listItemArray) {
-      const item = document.createElement("div");
-      item.innerText = list.list;
+      const listContainerElement = document.createElement("div");
+      const li = document.createElement("li");
+      const toDoText = document.createTextNode(list.item);
+      li.appendChild(toDoText);
+      listContainerElement.appendChild(li);
+      myToDoListElement.appendChild(listContainerElement);
+
+      //create mark button
+      const markButton = document.createElement("button");
+      markButton.innerHTML = "✔️";
+      markButton.addEventListener("click", markElement);
+      listContainerElement.appendChild(markButton);
+
+      //create delete button
+      const deleteButton = document.createElement("button");
+      deleteButton.innerHTML = "❌";
+      deleteButton.addEventListener("click", removeElement);
+      listContainerElement.appendChild(deleteButton);
     }
   }
 }
