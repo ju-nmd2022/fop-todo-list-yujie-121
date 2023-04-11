58070b09bab02ddd1d5565c14e79e66e576415cc
yujie-121 <94167770+yujie-121@users.noreply.github.com>
2023-04-11 23:38:11 +0200

    mark function have bug

--- test.js
+++ test.js
@@ -57,23 +57,24 @@ function onLoadHandler() {
   function saveToDoListToLocalStorage() {
     localStorage.setItem("toDoList", JSON.stringify(toDoList));
   }
-}
 
-function markElement() {
-  const element = this.parentNode;
-  element.style.textDecoration = "line-through";
-}
+  function markElement(index) {
+    const element = myToDoListElement.children[index];
+    element.style.textDecoration = "line-through";
+    saveToDoListToLocalStorage();
+  }
 
-function removeElement() {
-  const element = this.parentNode;
-  element.parentNode.removeChild(element);
+  function removeElement(index) {
+    toDoList.splice(index, 1);
+    renderToDoList();
+    saveToDoListToLocalStorage();
+  }
 }
 
 window.addEventListener("load", onLoadHandler);
 
 //The coding I used "forEach()"
-//"forEach()" method executes a provided function once for each element in the array in ascending order.
-//The provided function is called with three arguments: the current element of the array being processed, the index of the current element, and the array itself.
 
 //In my code, the forEach() method is being used to iterate over the toDoList array and render each to-do item on the screen.
-//The provided function takes two parameters: item and index. item refers to the current element being processed, which is an object that contains the text and state of a to-do item. index refers to the index of the current element in the array.
+//The provided function takes two parameters: item and index. item refers to the current element being processed
+//Which is an object that contains the text and state(marked or not) of a to-do item. index refers to the index of the current element in the array.
